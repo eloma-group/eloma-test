5496fdfca28d43bb09b4cb0c0b1d7bf72f4ddcd7
sawan-1201 <sawan_2002148@gita.edu.in>
2026-06-04 11:33:58 +0530

    initial: scaffold elomagroup-landing from bivry-landing project

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { HomePage } from './pages/HomePage'
+import { ContactPage } from './pages/ContactPage'
+import { AboutPage } from './pages/AboutPage'
+import { ValuesPage } from './pages/ValuesPage'
+import { NetworksPage } from './pages/NetworksPage'
+import { FAQPage } from './pages/FAQPage'
+import { CaseStudiesPage } from './pages/CaseStudiesPage'
+import { BlogsPage } from './pages/BlogsPage'
+import { NewsPage } from './pages/NewsPage'
+import { CareersPage } from './pages/CareersPage'
+import { IndustryPage } from './pages/IndustryPage'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/values" element={<ValuesPage />} />
+        <Route path="/networks" element={<NetworksPage />} />
+        <Route path="/faq" element={<FAQPage />} />
+        <Route path="/case-studies" element={<CaseStudiesPage />} />
+        <Route path="/blog" element={<BlogsPage />} />
+        <Route path="/news" element={<NewsPage />} />
+        <Route path="/careers" element={<CareersPage />} />
+        <Route path="/industries/:slug" element={<IndustryPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
